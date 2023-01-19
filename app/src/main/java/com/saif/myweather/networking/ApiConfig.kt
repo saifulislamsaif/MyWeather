04e3a3994292567ae7d26ca10bf88161813819cb
saiful islam <saifulislambaust28@gmail.com>
2023-01-19 11:46:19 +0600

    api config added

---
+++ app/src/main/java/com/saif/myweather/networking/ApiConfig.kt
@@ -0,0 +1,33 @@
+package com.saif.myweather.networking
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+    companion object {
+        fun getApiService(): ApiService {
+
+            // API response interceptor
+            val loggingInterceptor = HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            // Client
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            // Retrofit
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.weatherapi.com/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+
+        const val API_KEY = " fe4ddc2318874ecbac354554231701 "
+    }
+}
